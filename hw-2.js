554766740646ebb971da41f43b6e2710c30f51bc
Ivan Bulychev <psmivan@gmail.com>
2023-10-05 16:27:07 +0300

    Домашняя работа

---
+++ hw-2.js
@@ -0,0 +1,78 @@
+
+// Задание 1
+let a;
+a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+let IphoneBirth;
+IphoneBirth = 2007;
+alert(IphoneBirth);
+
+// Задание 3
+let FatherJs;
+FatherJs = "Брендан Эйх";
+alert(FatherJs);
+
+// Задание 4
+let b=10,c=2;
+alert(b+c);
+alert(b-c);
+alert(b*c);
+alert(b/c);
+
+// Задание 5
+let result = 2 ** 5;
+alert(result);
+
+// Задание 6
+a = 9;
+b = 2;
+alert(9%2);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= - 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age;
+age = prompt('Сколько Вам лет?');
+alert(age);
+
+// Задание 9
+const user = {
+    name: 'Ivan',
+    age: 30,
+    isAdmin: true,
+}
+
+// Задание 9.1
+user['city of residence'] = 'Russia';
+
+// Задание 9.2
+user.age = 25;
+
+// Задание 9.3
+delete user['city of residence'];
+
+// Задание 9.4
+let info;
+info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[`${info}`]);
+
+// Задание 10
+let name = prompt('Как Вас зовут?');
+alert('Привет, ' + `${name}` + '!');
+
+
+
+
+
