3d5da257a1dafdc3ad0fce463795624ab4f1cc8f
Ivan Bulychev <psmivan@gmail.com>
2023-10-20 19:34:35 +0300

    Домашняя работа

--- seasons.js
+++ seasons.js
@@ -1,14 +1,14 @@
 const seasons = () => {
     let numberMonth = Number(prompt("Введите номер месяца"));
     if (numberMonth === 12 || (numberMonth < 3 && numberMonth > 0)) {
-        console.log("Зима");
+        alert("Зима");
     } else if (numberMonth > 2 && numberMonth < 6) {
-        console.log("Весна");
+        alert("Весна");
     } else if (numberMonth > 5 && numberMonth < 9) {
-        console.log("Лето");
+        alert("Лето");
     } else if (numberMonth > 8 && numberMonth <12) {
-        console.log("Осень"); 
+        alert("Осень"); 
     } else {
-        console.log("Вы ввели не существующий месяц")
+        alert("Вы ввели не существующий месяц")
     }
 }
